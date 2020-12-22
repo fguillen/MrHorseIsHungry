d3ac38ae5d885be8386c6d7e131622b7fac0e92e
Fernando Guillen <fguillen.mail@gmail.com>
2020-12-22 20:03:30 +0100

    MrHorse starts walking

---
+++ MrHorseIsGoingHome/Assets/MrHorseController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MrHorseController : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody2D rb;
+    float originalScaleX;
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        originalScaleX = transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        if(horizontal != 0)
+        {
+            animator.SetBool("walking", true);
+        } else
+        {
+            animator.SetBool("walking", false);
+        }
+
+        if(horizontal < 0)
+        {
+            transform.localScale = new Vector3(-originalScaleX, transform.localScale.y, transform.localScale.z);
+        } else if(horizontal > 0)
+        {
+             transform.localScale = new Vector3(originalScaleX, transform.localScale.y, transform.localScale.z);
+        }
+
+        rb.velocity = new Vector2(horizontal * speed, 0);
+    }
+}
