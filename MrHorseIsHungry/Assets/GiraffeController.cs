77bd14aeb2fc57a2f72522816a198d724d2124cb
Fernando Guillen <fguillen.mail@gmail.com>
2020-12-26 19:13:13 +0100

    Giraffe is offering an apple

---
+++ MrHorseIsHungry/Assets/GiraffeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiraffeController : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Fire
+        if(Input.GetButtonDown("Jump"))
+        {
+            OfferApple();
+        }
+    }
+
+    void OfferApple()
+    {
+        animator.SetTrigger("offeringApple");
+    }
+}
