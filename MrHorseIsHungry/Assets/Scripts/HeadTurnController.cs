417a40c4f40866f7ba045648127cf4bb42a3577a
Fernando Guillen <fguillen.mail@gmail.com>
2021-01-10 10:56:57 +0100

    MrElephant is also turning the head

---
+++ MrHorseIsHungry/Assets/Scripts/HeadTurnController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadTurnController : MonoBehaviour
+{
+    // Update is called once per frame
+    Animator animator;
+    bool headPositionBackwards;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        headPositionBackwards = false;
+    }
+
+    void Update()
+    {
+        CheckHeadPosition();
+    }
+
+    void CheckHeadPosition()
+    {
+        float distanceToMrHorse = ObjectsReferrer.instance.mrHorseController.gameObject.transform.position.x - gameObject.transform.position.x;
+
+        if(distanceToMrHorse > 5 && !headPositionBackwards)
+        {
+            animator.SetBool("headBackwards", true);
+            headPositionBackwards = true;
+            
+        } else if(distanceToMrHorse < -2 && headPositionBackwards)
+        {
+            headPositionBackwards = false;
+            animator.SetBool("headBackwards", false);
+        }
+    }
+}
