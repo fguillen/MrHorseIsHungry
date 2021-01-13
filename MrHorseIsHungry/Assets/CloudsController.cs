4ea83bcaa2198edc2d0c4402b13197f0dedd3b64
Fernando Guillen <fguillen.mail@gmail.com>
2021-01-13 20:23:38 +0100

    Details with the cameras

---
+++ MrHorseIsHungry/Assets/CloudsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudsController : MonoBehaviour
+{
+    [SerializeField] Transform targetToFollow;
+    float xOffset;
+
+    void Start()
+    {
+        xOffset = transform.position.x - targetToFollow.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = new Vector3(targetToFollow.position.x + xOffset, transform.position.y, transform.position.y);
+        transform.position = position;
+    }
+}
