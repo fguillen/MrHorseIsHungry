fbf6536fbb026a7b063f7131f2726e683fdbf2ed
Fernando Guillen <fguillen.mail@gmail.com>
2021-01-15 14:52:43 +0100

    Menu Scene version 2 (with clouds)

---
+++ MrHorseIsHungry/Assets/Scripts/UIButtonsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIButtonsController : MonoBehaviour
+{
+    void LoadSceneGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    void LoadSceneMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    void ShowCredits()
+    {
+        print("ShowCredits");
+    }
+
+    void Quit()
+    {
+        Application.Quit();
+    }
+}
